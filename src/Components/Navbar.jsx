3153a62d506c04e46f0af3bf7401414421389fb2
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-22 18:03:11 -0300

    Add custom hook to Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,14 +1,21 @@
+import { Link } from 'react-router-dom';
 import React from 'react';
 import Search from './Search';
-import { Link } from 'react-router-dom';
+import SearchResult from './SearchResult';
+import useSearchMeal from '../Hooks/useSearchMeal';
 
-export default function Navbar({search}){
+export default function Navbar(){
+
+    const { search,handlerSearch, searchResult, setHandlerSearch, setSearchResult} = useSearchMeal();
 
 
     return (
         <div className='navbar'>
             <Link to='/'><h1>Recipe App</h1></Link>
-            <Search search={search}/>
+            <Search search={search} />
+            { handlerSearch ?
+                <SearchResult result={searchResult} setHandlerSearch={setHandlerSearch} setSearchResult={setSearchResult}/> : <></>
+            }
         </div>
     )
 }
