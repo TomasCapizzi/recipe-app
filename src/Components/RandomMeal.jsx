41d04e0e19e65fc23b9d69d64897f466b5153de4
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-22 18:03:56 -0300

    add new component

--- src/Components/RandomMeal.jsx
+++ src/Components/RandomMeal.jsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import {AiOutlineReload} from 'react-icons/ai';
 import Preview from './Preview';
-
+import RandomHeader from './RandomHeader';
+import React from 'react';
 
 export default function RandomMeal({random, randomMeal}){
 
     return (
-        <>
         <div className='random-container'>
-            <div className='app-header'>
-             <h2>Ideas to inspire you</h2>
-             <button onClick={randomMeal}><AiOutlineReload/></button>
-            </div>
+            <RandomHeader randomMeal={randomMeal} />
             <Preview meal={random}/>
         </div>
-        </>
-        )
+    )
 }
