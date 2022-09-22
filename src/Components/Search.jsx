69176838d5c4c6a2a07e4c9d8ebc1a81021b8687
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-22 18:04:32 -0300

    Update to use useRef

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import {BiSearch} from 'react-icons/bi'
+import React,{useRef} from 'react';
 
+import {BiSearch} from 'react-icons/bi'
 
 export default function Search({search}){
 
-function getSearch(e){
-       e.preventDefault();
-       let value = document.getElementById('search').value;
-       search(value);
-       document.getElementById('search').value = ''
-} 
+
+    const searchRef = useRef()
+
+    function getSearch(e){
+        e.preventDefault();
+        let value = searchRef.current.value;
+        search(value);
+        searchRef.current.value = ''
+    } 
 
     return (
         <div className='search'>
             <form action="" onSubmit={getSearch}>
-                <input type="text"  id='search'/>
+                <input type="text"  ref={searchRef}/>
                 <BiSearch className='search-logo' onClick={getSearch}/>
             </form>
-           
-
         </div>
     )
 }
