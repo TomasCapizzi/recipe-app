77d56af77986e36cf1b0ccdb7b114a6c8f5acd8b
Tomas Capizzi <tomas.capizzi@hotmail.com>
2022-09-22 18:03:34 -0300

    Change page indentation and create pages

--- src/App.js
+++ src/App.js
@@ -1,82 +1,40 @@
-import React,{useState,useEffect} from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import './Styles/main.scss'
-import FavMealsList from './Components/FavMealsList';
-import Footer from './Components/Footer';
-import Navbar from './Components/Navbar';
-import RandomMeal from './Components/RandomMeal';
-import Meal from './Components/Meal';
-import { FavContextProvider } from './Context/FavContext';
-import MenuCategories from './Components/MenuCategories';
-import Category from './Components/Category'
-import Spinner from './Components/Spinner/Spinner';
-import SearchResult from './Components/SearchResult';
 
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Category from './Pages/Category'
+import { FavContextProvider } from './Context/FavContext';
+import Footer from './Components/Footer';
+import Home from './Pages/Home';
+import MealDetail from './Pages/MealDetail';
+import MenuCategories from './Components/MenuCategories';
+import Navbar from './Components/Navbar';
+import React from 'react';
 
 function App() { 
 
-  const url = 'https://www.themealdb.com/api/json/v1/1';
-  const [random, setRandom] = useState();
-  const [searchResult, setSearchResult] = useState();
-  const [handlerSearch, setHandlerSearch] = useState(false);
-
-
-
-  async function randomMeal(){
-    const respuesta = await fetch(url + '/random.php');
-    const res = await respuesta.json();    
-    setRandom(res.meals)
-  }
-  async function search(term){
-    const respuesta = await fetch(url + '/search.php?s=' + term);
-    const res = await respuesta.json();
-    const food = res.meals;
-    setSearchResult(food);
-    setHandlerSearch(true);   
-}
-
-
-  useEffect(()=>{
-    randomMeal();
-  },[]) 
 
 
   return (
     <FavContextProvider>
-    <BrowserRouter>
-    <div className='app'>
-    <Navbar search={search}/>
-    <MenuCategories/>
-    <Switch>
-      <Route exact path='/'>
-        <FavMealsList/>
-        { random ?
-          <RandomMeal random={random} randomMeal={randomMeal}/> : <><Spinner/></>
-        }
-        { handlerSearch ?
-            <SearchResult result={searchResult} setHandlerSearch={setHandlerSearch} setSearchResult={setSearchResult}/> : <></>
-        }
-      </Route>
-      <Route path='/meal/:id'>
-      { random &&
-          <Meal url={url}/>
-        }
-      { handlerSearch ?
-            <SearchResult result={searchResult} setHandlerSearch={setHandlerSearch} setSearchResult={setSearchResult}/> : <></>
-        }
-      </Route>
-      <Route path='/category/:cat'>
-        <Category/>
-        { handlerSearch ?
-            <SearchResult result={searchResult} setHandlerSearch={setHandlerSearch} setSearchResult={setSearchResult}/> : <></>
-        }
-      </Route>
-
-    </Switch>
-
-    <Footer/>
-    </div>
-    </BrowserRouter>
+      <BrowserRouter>
+      <div className='app'>
+        <Navbar />
+        <MenuCategories/>
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route path='/meal/:id'>
+            <MealDetail/>
+          </Route>
+          <Route path='/category/:cat'>
+            <Category/>
+          </Route>
+        </Switch>
+        <Footer/>
+      </div>
+      </BrowserRouter>
     </FavContextProvider>
   );
 }
