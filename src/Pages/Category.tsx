e07e6e3bc919c1c97fc3aeb55c16034897404991
Tomas Capizzi <tomas.capizzi@hotmail.com>
2023-06-07 23:11:18 -0300

    Migrate Pages to TS

--- src/Pages/Category.tsx
+++ src/Pages/Category.tsx
@@ -1,16 +1,19 @@
 import React,{useEffect, useState} from 'react';
 
+import { Meal } from '../Interfaces/Meal';
 import PreviewCat from '../Components/PreviewCat';
-import SearchResult from '../Components/SearchResult';
 import Spinner from '../Components/Spinner/Spinner';
 import { useParams } from 'react-router';
-import useSearchMeal from '../Hooks/useSearchMeal';
+
+//import useSearchMeal from '../Hooks/useSearchMeal';
+
+//import SearchResult from '../Components/SearchResult';
 
 export default function Categories(){
-    const url = 'https://www.themealdb.com/api/json/v1/1';
+    const url: string = 'https://www.themealdb.com/api/json/v1/1';
 
     const {cat} = useParams();
-    const [meals, setMeals] = useState();
+    const [meals, setMeals] = useState<Meal[]>();
 
     async function getCategoryMeals(){
         const respuesta = await fetch(url + '/filter.php?c=' + cat)
@@ -19,7 +22,7 @@ export default function Categories(){
     }
 
     useEffect(()=>{
-        setMeals()
+        setMeals([])
         getCategoryMeals()
          // eslint-disable-next-line react-hooks/exhaustive-deps
     },[cat])
@@ -28,7 +31,7 @@ export default function Categories(){
         <div className='category-container'>
             <h3>{cat}</h3>
             {
-                meals ? meals.map(meal=> <PreviewCat meal={meal} key={meal.idMeal}/>) : <><Spinner/></>
+                meals ? meals.map(meal=> <PreviewCat meal={meal} key={meal.idMeal}/>) : <Spinner/>
             }
         </div>
     )
