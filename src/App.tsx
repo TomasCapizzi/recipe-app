9d2a8105bfaa22f2952bf12c323798d041b7f71a
Tomas Capizzi <tomas.capizzi@hotmail.com>
2023-06-07 23:07:21 -0300

    Migrate App to TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import './Styles/main.scss'
 
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Category from './Pages/Category'
+import Category from './Pages/Category';
 import { FavContextProvider } from './Context/FavContext';
 import Footer from './Components/Footer';
 import Home from './Pages/Home';
@@ -13,27 +13,19 @@ import React from 'react';
 
 function App() { 
 
-
-
   return (
     <FavContextProvider>
       <BrowserRouter>
-      <div className='app'>
-        <Navbar />
-        <MenuCategories/>
-        <Switch>
-          <Route exact path='/'>
-            <Home />
-          </Route>
-          <Route path='/meal/:id'>
-            <MealDetail/>
-          </Route>
-          <Route path='/category/:cat'>
-            <Category/>
-          </Route>
-        </Switch>
-        <Footer/>
-      </div>
+        <div className='app'>
+          <Navbar />
+          <MenuCategories/>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/meal/:id' element={<MealDetail/>}/>
+            <Route path='/category/:cat' element={<Category/>} />
+          </Routes>
+          <Footer/>
+        </div>
       </BrowserRouter>
     </FavContextProvider>
   );
